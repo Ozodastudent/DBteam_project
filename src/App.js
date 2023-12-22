1098daa9b99b62533dbf955cebca8040cc9b8394
Ozoda <shakhobiddinovaozoda@yandex.com>
2023-12-22 11:38:58 +0500

    shdjd

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
-import About from "./pages/about/About";
-import Listings from "./pages/listings/Listing";
-import Services from "./pages/services/Services";
-import Blogs from "./pages/blogs/Blogs";
-import ErrorPage from "./pages/error/Error";
 import { Login, Register } from "./pages/login/Login";
 
 
@@ -15,9 +10,6 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/listings" element={<Listings />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/blogs" element={<Blogs />} />
       </Routes>
     </Router>
   );
